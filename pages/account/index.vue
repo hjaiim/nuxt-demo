8c509a0305b0caf217afc17cedca19c5401eefa5
hjai <hjaiim@163.com>
2018-11-02 11:32:56 +0800

    nuxt-错误路由

---
+++ pages/account/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+      account
+  </div>
+</template>
+
+<script>
+export default {
+  created(){},
+  mounted(){},
+  data(){
+    return {
+    }
+  },
+  components:{},
+  props:{},
+  watch:{},
+  computed:{},
+  methods:{},
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,9 @@
 <template>
   <section class="container">
+    <div><nuxt-link to="/account">账号</nuxt-link></div>
+    <div><nuxt-link to="/book">404</nuxt-link></div>
     <div>
       <logo/>
-      <h1 class="title">
-        nuxt-demo
-      </h1>
-      <h2 class="subtitle">
-        My majestic Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
     </div>
   </section>
 </template>
